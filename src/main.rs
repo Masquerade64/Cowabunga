125c0becbb581289046fee5f873746e5475d0b0b
Masquerade <87361986+Masquerade64@users.noreply.github.com>
2022-11-18 17:45:55 +0000

    Add files via upload
    Added support for Atari 50

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 use std::fs;
 use std::fs::File;
 use std::io::{BufReader, BufWriter, Read, Write};
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 struct Cli {
     input: String,
     output: String,
+    #[arg(short, long, value_enum, default_value_t = Key::Cowabunga)]
+    key: Key,
+}
+
+#[derive(Clone, ValueEnum, Copy)]
+enum Key {
+    Cowabunga = 0x3F04B286,
+    Atari = 0x2EEA4C8B
 }
 
 fn main() {
@@ -28,7 +36,7 @@ fn main() {
     for index in (0..file_size).step_by(0x10000) {
         file_reader.read_exact(&mut buffer).unwrap();
         let mut vec = buffer.to_vec();
-        decrypt_block(&mut vec, index as u32);
+        decrypt_block(&mut vec, index as u32, cli.key);
         output_file_writer.write_all(vec.as_slice()).unwrap();
     }
 
@@ -38,25 +46,25 @@ fn main() {
         file_reader.read_exact(&mut buffer).unwrap();
         vec.push(buffer[0]);
     }
-    decrypt_block(&mut vec, file_size as u32);
+    decrypt_block(&mut vec, file_size as u32, cli.key);
     output_file_writer.write_all(vec.as_slice()).unwrap();
 }
 
-fn decrypt_block(block: &mut Vec<u8>, offset_in_file: u32) {
+fn decrypt_block(block: &mut Vec<u8>, offset_in_file: u32, game_key: Key) {
     let mut sum = offset_in_file.wrapping_mul(0xCC9E2D51);
-    let mut key = get_key( (offset_in_file & 0xFFFFFFFC).wrapping_mul(0xCC9E2D51));
+    let mut key = get_key( (offset_in_file & 0xFFFFFFFC).wrapping_mul(0xCC9E2D51), game_key);
     for index in 0..block.len() {
         let iter = (offset_in_file.wrapping_add( index as u32) & 3) << 3;
         block[index] ^= (key >> iter) as u8;
         sum = sum.wrapping_sub(0x3361D2AF);
         if iter == 24 {
-            key = get_key(sum);
+            key = get_key(sum, game_key);
         }
     }
 }
 
-fn get_key(sum: u32) -> u32 {
-    let temp1 = sum.rotate_left(15).wrapping_mul(0x1B873593) ^ 0x3F04B286;
+fn get_key(sum: u32, game_key: Key) -> u32 {
+    let temp1 = sum.rotate_left(15).wrapping_mul(0x1B873593) ^ (game_key as u32);
     let temp2 = temp1.rotate_left(13).wrapping_mul(5).wrapping_sub(0x19AB949C);
     let temp3 = (temp2 ^ 0x40000) >> 16;
     let temp4 = (temp2 ^ temp3).wrapping_mul(0x85EBCA6B);
