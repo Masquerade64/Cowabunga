9e3015e6e682f86119f3f3253123e18766d3206f
Luciano Ciccariello <xeeynamo@hotmail.com>
2026-02-14 00:46:16 +0000

    Add Linux and macOS support

--- build.rs
+++ build.rs
@@ -1,7 +1,11 @@
+#[cfg(target_os = "windows")]
 extern crate winres;
 
 fn main() {
-    let mut res = winres::WindowsResource::new();
-    res.set_icon("icon.ico");
-    res.compile().unwrap();
+    #[cfg(target_os = "windows")]
+    {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("icon.ico");
+        res.compile().unwrap();
+    }
 }
